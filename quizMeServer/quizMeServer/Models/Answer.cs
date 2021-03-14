f5f27cacd910ac8f371ebcbe0134ebeac4e1e175
Betina <hristova.b.betina@gmail.com>
2021-03-14 23:35:27 +0200

    Add server project, but this time with gitignore

---
+++ quizMeServer/quizMeServer/Models/Answer.cs
@@ -0,0 +1,16 @@
+﻿namespace quizMeServer.Models
+{
+    public class Answer
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+        public bool Correct { get; set; }
+
+        public Answer(string id, string value, bool correct)
+        {
+            Id = id;
+            Value = value;
+            Correct = correct;
+        }
+    }
+}
